0f49b0a9ea7141ae748db75cb5e93f3e6e8e180c
jimin <jjimini98@naver.com>
2021-05-16 21:36:21 +0900

    created  crawling branch

--- crawling/test_crawler.py
+++ crawling/test_crawler.py
@@ -1,3 +1,10 @@
-import requests
+# 일단 알라딘으로 크롤링 
+
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+
+html = urlopen("https://www.aladin.co.kr/shop/wproduct.aspx?ItemId=193931483")
+soup = BeautifulSoup(html, "html.parser")
+pr = soup.find_all("div", {"class":"Ere_prod_mconts_box"})
+
+print(pr)
