ecb566e94cb846014c9fdd1af2eb73b01dd51dd7
jimin <jjimini98@naver.com>
2021-05-21 18:30:02 +0900

    proceeding hanbit_crawler

--- crawling/test_crawler.py
+++ crawling/test_crawler.py
@@ -1,10 +1,22 @@
-# 일단 알라딘으로 크롤링 
-
-from bs4 import BeautifulSoup
 from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
-html = urlopen("https://www.aladin.co.kr/shop/wproduct.aspx?ItemId=193931483")
-soup = BeautifulSoup(html, "html.parser")
-pr = soup.find_all("div", {"class":"Ere_prod_mconts_box"})
+html = urlopen("https://www.hanbit.co.kr/media/books/book_view.html?p_code=B2649943562")
+bs = BeautifulSoup(html, 'html.parser')
+title = bs.select("#container > div.store_view_wrap > div.store_view_wrap_l > div.store_view_area > div.store_product_info_box > h3")
+print(title)
+# 텍스트만 꺼내야함
+# for t in title: 
+#    with open("./crawling/hanbit_crawling.txt","w") as f:
+#       f.write(t.text)
+#       f.write("\n")
 
-print(pr)
+# context = bs.select("#tabs_1 > div > p:nth-child(3)")
+
+# for t in context:
+#    with open("./crawling/hanbit_crawling.txt","a") as f:
+#       f.write(t.text)
+#       f.write("\n")
+
+
+   
