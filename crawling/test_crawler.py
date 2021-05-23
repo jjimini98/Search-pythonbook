868a2a963881a71a9803a8d6ab8649a1b96fafb1
jimin <jjimini98@naver.com>
2021-05-23 17:30:02 +0900

    한빛미디어 파이썬 책 크롤링 결과 : 되는 것도 있고 안되는 것도 있음. txt 확인 필요

--- crawling/test_crawler.py
+++ crawling/test_crawler.py
@@ -2,24 +2,27 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 
-def hanbit_crawler(url): 
+def hanbit_python_crawler(url): 
    site = urlopen(url)
    bs = BeautifulSoup(site, 'html.parser',from_encoding='utf-8')
    title = bs.select("#container > div.store_view_wrap > div.store_view_wrap_l > div.store_view_area > div.store_product_info_box > h3")
    for t in title: 
-      with open("./crawling/hanbit_crawling.txt","a", encoding='utf-8') as f:
+      with open("./crawling/hanbit_python_crawling.txt","a", encoding='utf-8') as f:
          f.write(t.text)
          f.write("\n")
 
    context = bs.select("#tabs_1 > div > p:nth-child(3)")
-   print("context is")
-   print(context)
    for t in context:
-      with open("./crawling/hanbit_crawling.txt","a", encoding='utf-8') as f:
+      with open("./crawling/hanbit_python_crawling.txt","a", encoding='utf-8') as f:
          f.write(t.text)
+         f.write("\n")
          f.write("\n")
 
 
 
 
-hanbit_crawler("https://www.hanbit.co.kr/media/books/book_view.html?p_code=B7267069633")
+with open("./crawling/hanbit_python_url.txt","r") as f :
+   while True:
+      line = f.readline()
+      if not line: break
+      hanbit_python_crawler(line)
