3f3b1f8e83c2d8d43b24be9890aa94247a710142
jimin <jjimini98@naver.com>
2021-05-23 17:03:23 +0900

    함수로 만들어서 재사용이 가능하게끔 수정

--- crawling/test_crawler.py
+++ crawling/test_crawler.py
@@ -1,22 +1,25 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-html = urlopen("https://www.hanbit.co.kr/media/books/book_view.html?p_code=B2649943562")
-bs = BeautifulSoup(html, 'html.parser')
-title = bs.select("#container > div.store_view_wrap > div.store_view_wrap_l > div.store_view_area > div.store_product_info_box > h3")
-print(title)
-# 텍스트만 꺼내야함
-# for t in title: 
-#    with open("./crawling/hanbit_crawling.txt","w") as f:
-#       f.write(t.text)
-#       f.write("\n")
 
-# context = bs.select("#tabs_1 > div > p:nth-child(3)")
+def hanbit_crawler(url): 
+   site = urlopen(url)
+   bs = BeautifulSoup(site, 'html.parser')
+   title = bs.select("#container > div.store_view_wrap > div.store_view_wrap_l > div.store_view_area > div.store_product_info_box > h3")
+   for t in title: 
+      with open("./crawling/hanbit_crawling.txt","w") as f:
+         f.write(t.text)
+         f.write("\n")
 
-# for t in context:
-#    with open("./crawling/hanbit_crawling.txt","a") as f:
-#       f.write(t.text)
-#       f.write("\n")
+   context = bs.select("#tabs_1 > div > p:nth-child(3)")
+   print("context is")
+   print(context)
+   # for t in context:
+   #    with open("./crawling/hanbit_crawling.txt","a") as f:
+   #       f.write(t.text)
+   #       f.write("\n")
 
 
-   
+
+
+hanbit_crawler("https://www.hanbit.co.kr/media/books/book_view.html?p_code=B7267069633")
