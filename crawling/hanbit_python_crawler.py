bc12e94608acfde6a0c48d93c1a4d7aebe310ddc
jimin <jjimini98@naver.com>
2021-05-23 18:46:54 +0900

    한빛미디어 파이썬 책 크롤링 완료

--- crawling/hanbit_python_crawler.py
+++ crawling/hanbit_python_crawler.py
@@ -11,13 +11,13 @@ def hanbit_python_crawler(url):
          f.write(t.text)
          f.write("\n")
 
+
    context = bs.select("#tabs_1 > div > p:nth-child(3)")
    for c in context:
       with open("./crawling/hanbit_python_crawling.txt","a", encoding='utf-8') as f:
-         f.write(c.text)
-         f.write("\n")
-         f.write("\n")
-
+            f.write(c.text)
+            f.write("\n")
+            f.write("\n")
 
 
 #url 모음에서 한 줄씩 읽어서 크롤러 함수에 인자로 넣음. 
@@ -27,6 +27,7 @@ with open("./crawling/hanbit_python_url.txt","r") as f :
       if not line: break
       hanbit_python_crawler(line)
 
+#크롤링할 때 생기는 빈칸들(제대로 크롤링되지 않은 것들)은 손수 삭제함.
 
 
 
