7126135f2b41bf4eae7f4cfe7419d255fcc8bfcf
jimin <jjimini98@naver.com>
2021-05-23 17:15:45 +0900

    인코딩 문제 해결

--- crawling/test_crawler.py
+++ crawling/test_crawler.py
@@ -4,20 +4,20 @@ from bs4 import BeautifulSoup
 
 def hanbit_crawler(url): 
    site = urlopen(url)
-   bs = BeautifulSoup(site, 'html.parser')
+   bs = BeautifulSoup(site, 'html.parser',from_encoding='utf-8')
    title = bs.select("#container > div.store_view_wrap > div.store_view_wrap_l > div.store_view_area > div.store_product_info_box > h3")
    for t in title: 
-      with open("./crawling/hanbit_crawling.txt","w") as f:
+      with open("./crawling/hanbit_crawling.txt","a", encoding='utf-8') as f:
          f.write(t.text)
          f.write("\n")
 
    context = bs.select("#tabs_1 > div > p:nth-child(3)")
    print("context is")
    print(context)
-   # for t in context:
-   #    with open("./crawling/hanbit_crawling.txt","a") as f:
-   #       f.write(t.text)
-   #       f.write("\n")
+   for t in context:
+      with open("./crawling/hanbit_crawling.txt","a", encoding='utf-8') as f:
+         f.write(t.text)
+         f.write("\n")
 
 
 
