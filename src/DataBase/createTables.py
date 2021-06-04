b3a5aa7b7e849907e51642e81fa856458d472816
jimin <jjimini98@naver.com>
2021-06-05 00:18:07 +0900

    [Backend] test db에 테이블 추가 / db_name 변수 변경 가능

--- src/DataBase/createTables.py
+++ src/DataBase/createTables.py
@@ -4,19 +4,21 @@ ip = '118.67.131.97'
 port = 3306
 user = 'root'
 pw = 'jimin980908'
+db_name = 'test'
 
 
-conn = pymysql.connect(host = ip, port = port , user = user , password= pw , db = 'pyinder')
+conn = pymysql.connect(host = ip, port = port , user = user , password= pw , db = db_name)
 
 cursor = conn.cursor()
 
-sql ='''CREATE TABLE Book ( 
+sql1 ='''CREATE TABLE Book ( 
    id INT NOT NULL AUTO_INCREMENT ,
    title VARCHAR(255) NOT NULL,
    picture BINARY,
-   intro VARCHAR(500) NOT NULL,
+   intro VARCHAR(1000) NOT NULL, 
    Tag_id INT NOT NULL,
    PRIMARY KEY (id) ) '''
+# intro의 데이터타입 500으로 부족해서 1000으로 늘림 
 
 
 sql2 = ''' CREATE TABLE Review ( 
@@ -47,12 +49,12 @@ sql6 = '''ALTER TABLE Book ADD CONSTRAINT Tag_Book FOREIGN KEY (Tag_id) REFERENC
 
 
 
-
-# cursor.execute(sql2)
-# cursor.execute(sql3)
-# cursor.execute(sql4)
-# cursor.execute(sql5)
-# cursor.execute(sql6)
+cursor.execute(sql1)
+cursor.execute(sql2)
+cursor.execute(sql3)
+cursor.execute(sql4)
+cursor.execute(sql5)
+cursor.execute(sql6)
 
 
 
