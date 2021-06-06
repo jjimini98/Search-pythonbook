fbe557b2c2931598b5aade886de59a6f5cbec6c8
jimin <jjimini98@naver.com>
2021-06-07 03:27:08 +0900

    [Backend] Tag Table에 데이터 저장

--- src/DataBase/insertTagTable.py
+++ src/DataBase/insertTagTable.py
@@ -9,4 +9,13 @@ db_name = 'pyinder'
 conn = pymysql.connect(host = ip, port = port , user = user , password= pw , db = db_name)
 cursor = conn.cursor()
 
-tag_list = ['초보자', ]
+tag_list = ["파이썬", "기초", "딥러닝", "초보", "수학", "머신러닝", "Tensorflow", "비전공자", "입문서", "자료구조", "중고급", "입문자","알고리즘", "중급"]
+
+sql = '''INSERT INTO Tag(name) VALUES (%s)'''
+
+
+for x in tag_list:
+   cursor.execute(sql,x)
+
+conn.commit()
+conn.close()
