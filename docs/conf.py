c362e9b83081f68d9b2918cc0fd0d7a76c2e3bc8
jimin <jjimini98@naver.com>
2021-06-13 18:41:19 +0900

    created sphinx documents

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ import os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, '/root/DataArchitecture')
 
 # -- General configuration ------------------------------------------------
 
@@ -68,7 +68,7 @@ release = ''
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = 'en'
+language = 'ko'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
