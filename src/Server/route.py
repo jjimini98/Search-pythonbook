454fbb456bfa45027cfe085f6e5f0c418e9f6b65
jimin <jjimini98@naver.com>
2021-06-13 18:30:02 +0900

    [Backend] 라우팅 경로 설정 끝

--- src/Server/route.py
+++ src/Server/route.py
@@ -8,37 +8,72 @@ user = 'root'
 pw = 'jimin980908'
 db_name = 'pyinder'
 
+conn = pymysql.connect(host=ip, port=port, user=user, password=pw, db=db_name)
+cursor = conn.cursor()
 
 
-@app.route('/',methods=['GET'])
-def main():
-    return render_template("home.html")
+from flask import Flask, render_template, request, redirect, url_for
+app = Flask(__name__)
+lis = []
+data = {}
+result = []
+
+@app.route('/')
+def index(): # 리스트(board)를 html 파일로 넘기기
+ return render_template('home.html', rows = lis)
+
+@app.route('/search', methods = ['POST'])
+def add():
+    if request.method == 'POST':
+        question = request.form['input']
+        lis.append(question)
+        data = question
+        sql = """insert into Search (keyword) values (%s)"""
+        cursor.executemany(sql,data)
+        conn.commit()
+
+        sql = '''SELECT title, intro FROM Book'''
+        cursor.execute(sql)
+        answer = cursor.fetchall()
+        for each in answer:
+            for x in each:
+                if question in x:
+                    result.append(x)
+
+    return render_template('search.html', data= [result, question])
 
 
-@app.route('/search', methods=['POST'])
-def search():
-    value = request.form['context']  # value는 값이 다음 페이지(search)로 넘어가게 됨
-    conn = pymysql.connect(host=ip, port=port, user=user, password=pw, db=db_name)
 
-    try:
-        db = conn
-        c = conn.cursor()
-        c.execute("select EXISTS (select * from Book where intro={}) as success;".format(value))
-        db.commit()
-    except Exception as e:
-        print('db error:', e)
-    finally: db.close()
+if __name__ == '__main__':
+    app.run()
 
 
 
 
 
 
-# result = []
-# @app.route('/search',methods=['GET'])
+# @app.route('/',methods=['GET'])
+# def main():
+#
+#     return render_template("home.html", data= "")
+#
+#
+# @app.route('/search', methods=['POST'])
 # def search():
-#       if (request.method == 'GET'):
-#          return render_template("home.html")
+#     result = []
+#     value = request.form['input'] # value는 값이 다음 페이지(search)로 넘어가게
+#
+#     sql = '''SELECT intro FROM Book'''
+#     cursor.execute(sql)
+#     answer = cursor.fetchall()
+#
+#     for x in answer:
+#         for t in x:
+#             if value in t:
+#                 result.append(t)
+#     return  render_template('search.html')
+
+
 
 
 
@@ -46,16 +81,3 @@ if __name__ == "__main__":
       app.run()
 
 
-   # if request.method == '': # home.html 에서 입력받은 date, input 를 가져와서 result 변수에 추가
-   #    # date = request.form['date']  #검색날짜 가져오기
-   #    input = request.form['input']
-   #    result.append([input])
-   #    # return redirect(url_for('result'))    #result 를 리다이렉트
-   #    return "success"
-   # # else: return render_template('home.html', rows = result)
-
-
-#
-#
-# if __name__ == "__main__":
-#     app.run()
